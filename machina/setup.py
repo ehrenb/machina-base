0735da445ad6c307b4ccb26da9507f17051d7793
ehrenb <branden.a.ehrenreich@gmail.com>
2023-02-19 02:42:10 -0500

    force unzip of .egg file, which allows mkdocstrings to parse docstrings from package source without needing to hard code its path

--- machina/setup.py
+++ machina/setup.py
@@ -4,11 +4,12 @@ from os import path
 setup(
     name='machina',
     version='0.1',
-    description='A scalable APK analysis pipeline',
+    description='A scalable analysis pipeline',
     author='',
     author_email='behren2@protonmail.com',
     keywords='machina',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     python_requires='>=3.5',
-    include_package_data = True,
+    include_package_data=True,
+    zip_safe=False # force eggs to be unzipped, otherwise mkdocstrings will fail when looking for module source (if the pkg was zipped into an egg)
 )
